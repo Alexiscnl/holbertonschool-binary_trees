aeb0229442f7f81fccbaae627d0a9094c7a58f6e
Alexiscnl <alexis.cornillon@gmail.com>
2025-04-07 14:37:25 +0200

    add all

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+/**
+ *
+ *
+ *
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL)
+		return;
+
+	if (tree == NULL)
+		return;
+
+	
+}
